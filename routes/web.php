9b6607669ce8f8069c4f4945008b23c66d10534c
rohanhacker <rohanvfx15@gmail.com>
2018-03-21 22:14:12 +0530

    dd() and dump() we use two method with collection

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,14 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+
+Route::get('/users',function (){
+   $users = \App\User::all();
+
+//    dd() dump after that code not work
+ //  return $users->sortByDesc('id')->dd()->where('created_at', '2018-03-21 16:20:13')->dump();
+
+//    dump() mean work as dd but not die; you can many time dump() method we use two time here
+    return $users->sortByDesc('id')->dump()->where('created_at', '2018-03-21 16:20:13')->dump();
+
+});
