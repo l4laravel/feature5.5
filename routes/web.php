a1a2e48c688b9b5e40c13f7fb1fe90d1eebec422
rohanhacker <rohanvfx15@gmail.com>
2018-03-22 19:46:05 +0530

    we can create view like this Route::view('/','welcome');  with router no need  like this right now return view('file name')

--- routes/web.php
+++ routes/web.php
@@ -11,57 +11,9 @@
 |
 */
 
-use App\Rules\ValidPhone;
+// we can create veiw in router
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::view('/','welcome');
 
 
-Route::get('/users',function (){
-   $users = \App\User::all();
-
-//    dd() dump after that code not work
- //  return $users->sortByDesc('id')->dd()->where('created_at', '2018-03-21 16:20:13')->dump();
-
-//    dump() mean work as dd but not die; you can many time dump() method we use two time here
-    return $users->sortByDesc('id')->dump()->where('created_at', '2018-03-21 16:20:13')->dump();
-
-});
-
-
-Route::get('/add',function (){
-
-    return view('add_user');
-});
-
-
-Route::post('add-user',function (\Illuminate\Http\Request $request){
-
-    // this is old way
-/*    $this->validate($request,[
-       'name' => 'required',
-       'email' =>'required',
-       'password' =>'required'
-
-    ]);*/
-
-    // new way right now
-
-    $request->validate([
-        'name' => 'required',
-        'email' =>'required',
-        'password' =>'required',
-        'phone' => ['required', new ValidPhone]
-    ]);
-
-
-});
 
